c3b9a762453681181176d6654d9987f4a359611d
Hukum Kumar <111696853+Itskartike@users.noreply.github.com>
2024-01-14 12:20:49 +0530

    Update link1.js

--- IQ Test/IQ Test/link1.js
+++ IQ Test/IQ Test/link1.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzU9l0XOyeMDe1OXQVw2vIjlETbUSa8mr6Fcr-t0yHlgah13u-JJsflQoF88KehSVJC/exec'
+const scriptURL = 'Put your script here'
 const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
 e.preventDefault()
